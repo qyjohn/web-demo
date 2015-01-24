160ab8517f4cb0f3b48b00d7c4a076695471b817
Jiang <johnjian@u3417eb9eb21553a2be12.ant.amazon.com>
2015-01-24 15:34:36 +1100

    first commit

---
+++ v0.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ *
+ * The first part handles application logic.
+ *
+ */
+
+
+session_start();
+
+if (isset($_POST['username'])) 
+{
+	// This is a login request
+	$username = $_POST['username'];
+	process_login($username);
+}
+
+if (isset($_GET['logout'])
+{
+	// This is a logout request
+	process_logout();
+}
+
+
+function process_login($username)
+{
+	$_SESSION['username'] = $username;
+}
+
+function process_logout()
+{
+	session_unset();
+	session_destroy();
+}
+
+
+$server = $_SERVER['REMOTE_ADDR'];
+?>
+
+<?
+/*
+ *
+ * The second part handles user interface.
+ *
+ */
+
+if (isset($_SESSION['username']))
+{
+	echo "<H1>$server</H1>";
+}
+else
+{
+	echo "<H1>$server</H1>";
+}
+
+?>
