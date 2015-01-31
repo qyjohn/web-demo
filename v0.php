7b53bed67ae2d2c7bfbc119986e3dbd26da98ba4
Jiang <johnjian@u3417eb9eb21553a2be12.ant.amazon.com>
2015-01-31 15:50:51 +1100

    Ground 0 completed

--- v0.php
+++ v0.php
@@ -7,13 +7,17 @@
 
 
 session_start();
-include('db.php');
+$server   = $_SERVER['REMOTE_ADDR'];
+$username = $_SESSION['username'];
+$db = open_db_connection();
+
 
 if (isset($_POST['username'])) 
 {
 	// This is a login request
-	$username = $_POST['username'];
-	process_login($username);
+	process_login($_POST['username']);
+	// Get the new username
+	$username = $_SESSION['username'];
 }
 
 if (isset($_GET['logout']))
@@ -22,20 +26,69 @@ if (isset($_GET['logout']))
 	process_logout();
 }
 
+if (isset($_FILES["fileToUpload"]))
+{
+	// This is a upload request, save the file first
+	process_upload($_FILES["fileToUpload"]);
+	// Then write a record to the database
+	$filename = $_FILES["fileToUpload"]["name"];
+	add_upload_info($db, $username, $filename);
+}
+
 
 function process_login($username)
 {
+	// Simply write username to session data
 	$_SESSION['username'] = $username;
 }
 
 function process_logout()
 {
+	// Simply destroy session data
 	session_unset();
 	session_destroy();
 }
 
+function process_upload($uploadedFile)
+{
+	// Copy the uploaded file to "uploads" folder
+	$filename = $uploadedFile["name"];
+	$tgtFile  = "uploads/".$filename;	
+	move_uploaded_file($uploadedFile["tmp_name"], $tgtFile);
+}
 
-$server = $_SERVER['REMOTE_ADDR'];
+function open_db_connection()
+{
+	// Open a connection to the database
+	$hostname = 'localhost';
+	$database = 'web_demo';
+	$username = 'username';
+	$password = 'password';
+	$db = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password);
+	return $db;
+}
+
+function add_upload_info($db, $username, $filename)
+{
+	// Add a new record to the upload_images table
+	$sql = "INSERT INTO upload_images (username, filename) VALUES (?, ?)";	
+	$statement = $db->prepare($sql);
+	$statement->execute(array($username, $filename));
+}
+
+function retrieve_recent_uploads($db, $count)
+{
+	// Geting the latest records from the upload_images table
+	$sql = "SELECT * FROM upload_images ORDER BY timeline LIMIT $count";
+	$statement = $db->prepare($sql);
+	$statement->execute();
+	$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+	return $rows;
+}
+
+// This statement get the last 10 records from the database
+$images = retrieve_recent_uploads($db, 10);
+
 ?>
 
 <?php
@@ -47,11 +100,51 @@ $server = $_SERVER['REMOTE_ADDR'];
 
 if (isset($_SESSION['username']))
 {
-	echo '<H1>$server</H1><HR>';
+	// This section is shown when user is login
+	echo "<table width=100% border=0>";
+	echo "<tr>";
+		echo "<td><H1>$server</H1></td>";
+		echo "<td align='right'>";
+			echo "$username<br>";
+			echo "<a href='v0.php?logout=yes'>Logout</a>";
+		echo "</td>";
+	echo "</tr>";
+	echo "</table>";
+	echo "<HR>";
+
+	echo "In this crappy demo, we assume that you are uploading images files with file extensions such as JPG, JPEG, GIF, PNG.<br>&nbsp;<br>";
+
+	echo "<form action='v0.php' method='post' enctype='multipart/form-data'>";
+	echo "<input type='file' name='fileToUpload' id='fileToUpload'>";
+	echo "<input type='submit' value='Upload Image' name='submit'>";
+	echo "</form>";
+
 }
 else
 {
-	echo '<H1>$server</H1><HR>';
+	// This section is shown when user is not login
+	echo "<table width=100% border=0>";
+	echo "<tr>";
+		echo "<td><H1>$server</H1></td>";
+		echo "<td align='right'>";
+			echo "<form action='v0.php' method='post'>";
+			echo "Enter Your Name: <br>";
+			echo "<input type='text' id='username' name ='username' size=20><br>";
+			echo "<input type='submit' value='login'/>";
+			echo "</form>";
+		echo "</td>";
+	echo "</tr>";
+	echo "</table>";
+	echo "<HR>";
+}
+
+// Display the images
+echo "<br>&nbsp;<br>";
+foreach ($images as $image)
+{
+	$filename = $image["filename"];
+	$url = "uploads/".$filename;
+	echo "<img src='$url' width=200px height=150px>&nbsp;&nbsp;";
 }
 
 ?>
---
+++ web_demo.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: web_demo
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `upload_images`
+--
+
+DROP TABLE IF EXISTS `upload_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `upload_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT '',
+  `filename` varchar(255) DEFAULT '',
+  `timeline` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `upload_images`
+--
+
+LOCK TABLES `upload_images` WRITE;
+/*!40000 ALTER TABLE `upload_images` DISABLE KEYS */;
+INSERT INTO `upload_images` VALUES (1,'Qingye Jiang','clouds1.jpg','2015-01-31 04:21:11'),(2,'Qingye Jiang','clouds2.jpg','2015-01-31 04:21:15'),(3,'Qingye Jiang','clouds3.jpg','2015-01-31 04:21:20'),(4,'Qingye Jiang','clouds4.jpg','2015-01-31 04:21:25'),(5,'Qingye Jiang','clouds5.jpg','2015-01-31 04:24:26'),(6,'Qingye Jiang','clouds6.jpg','2015-01-31 04:24:30'),(7,'Qingye Jiang','clouds7.jpg','2015-01-31 04:24:34'),(8,'Qingye Jiang','clouds8.jpg','2015-01-31 04:24:38'),(9,'Qingye Jiang','clouds9.jpg','2015-01-31 04:25:49'),(10,'Qingye Jiang','clouds10.jpg','2015-01-31 04:25:53');
+/*!40000 ALTER TABLE `upload_images` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-01-31 15:50:13
