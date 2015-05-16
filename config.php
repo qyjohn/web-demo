8f754551a3e13e9a728322b8d689d08d9fa93d25
Ubuntu <ubuntu@ip-172-31-5-137.ap-southeast-2.compute.internal>
2015-05-16 01:19:06 +0000

    add S3 region

--- config.php
+++ config.php
@@ -12,11 +12,12 @@ $db_password = "password";
 // Image upload options
 $storage_option = "hd";
 $hd_folder  = "uploads";
-$s3_bucket  = "my_uploads_bucket";
+$s3_region  = "ap-southeast-2";
+$s3_bucket  = "my-upload-bucket";
 $s3_baseurl = "https://s3-ap-southeast-2.amazonaws.com/";
 if ($storage_option == "s3")
 {
-	$s3_client = S3Client::factory();
+	$s3_client = S3Client::factory(array('region' => $s3_region));
 }
 
 // Simulate latency, in seconds
