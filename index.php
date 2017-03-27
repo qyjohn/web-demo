8dc3700877651fef2fc1af836c01bb050f16ac27
Qingye Jiang (John) <qjiang@ieee.org>
2017-03-27 14:47:15 +1000

    Update index.php

--- index.php
+++ index.php
@@ -74,8 +74,18 @@ function process_login($username)
 
 function process_logout()
 {
-	// Simply destroy session data
-	session_unset();
+	// Unset all of the session variables.
+	$_SESSION = array();
+
+	// If it's desired to kill the session, also delete the session cookie.
+	// Note: This will destroy the session, and not just the session data!
+	if (ini_get("session.use_cookies")) 
+	{
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+	}
+
+	// Finally, destroy the session.
 	session_destroy();
 }
 
